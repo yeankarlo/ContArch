855b7379dac836aea13833cc70c32ac52cc5d17e
Juan Carlos VO <yeankarlo@gmail.com>
2013-04-06 14:17:47 -0600

    Correción de bloque
    Correción de cierre del SAVE y del cierre del TAB.

--- js/actions.js
+++ js/actions.js
@@ -7,33 +7,33 @@ $(document).ready(function(e) {
 			var tel = $('#cTel').val();
 			var mov = $('#cMov').val();
 			var mail = $('#cMail').val();
-		});
-		// ------crear el objeto de contacto para llenarlo con los datos
-		var nueCont = navigator.contacts.create();
-		nueCont.displayname = nom;
-		nueCont.nickname = nom;
-		// -----crear objeto para nombre del contacto; propiedad NAME del objeto de CONTACTOS
-		var nombre = new ContactName();
-		nombre.givenName = nom;
-		nueCont.name = nombre;
-		// ------crear el arreglo de telefonos; propiedad 
-		var telefonos = [];
-		telefonos[0] = new ContactField('home', tel, false);
-		telefonos[1] = new ContactField('mobile', mov, true); // preferred number
-		nueCont.phoneNumbers = telefonos;
-		// ------crear el arreglo para correos electrónicos
-		var correos = [];
-		correos[0] = new ContactField('home',mail,true); // preferred number
-		nueCont.emails = correos;
-		// ------
-		nueCont.save(function(){
-			$('#cNom,#cTel,#cMov,#cMail').val();
-			navigator.notification.alert('Creado satisfactoriamente', function(){
-					history.back();
-				},'Error al crear el contacto', 'Aceptar');
-		}, function(err){
-			navigator.notification.alert(err.code, null,'Error al crear el contacto', 'Aceptar');	
-		});
 		
+			// ------crear el objeto de contacto para llenarlo con los datos
+			var nueCont = navigator.contacts.create();
+			nueCont.displayname = nom;
+			nueCont.nickname = nom;
+			// -----crear objeto para nombre del contacto; propiedad NAME del objeto de CONTACTOS
+			var nombre = new ContactName();
+			nombre.givenName = nom;
+			nueCont.name = nombre;
+			// ------crear el arreglo de telefonos; propiedad 
+			var telefonos = [];
+			telefonos[0] = new ContactField('home', tel, false);
+			telefonos[1] = new ContactField('mobile', mov, true); // preferred number
+			nueCont.phoneNumbers = telefonos;
+			// ------crear el arreglo para correos electrónicos
+			var correos = [];
+			correos[0] = new ContactField('home',mail,true); // preferred number
+			nueCont.emails = correos;
+			// ------guardar el contacto
+			nueCont.save(function(){
+				$('#cNom,#cTel,#cMov,#cMail').val();
+				navigator.notification.alert('Creado satisfactoriamente', function(){
+						history.back();
+					},'Contacto OK', 'Aceptar');
+			}, function(err){
+				navigator.notification.alert(err.code, null,'Error al crear el contacto', 'Aceptar');	
+			});
+		});	
 	}, false);
 });
